a2b6ba7d344ebedbd846bb5bd6842238ff44c6d2
drodriguezr-lab <drodriguezr@ciencias.unam.mx>
2026-05-27 18:02:07 -0600

    constate K.py

--- models.py
+++ models.py
@@ -1,1 +1,11 @@
 
+import numpy as np 
+
+
+#Calculo de la constante K del modelo W = K L^3.
+def calculoconstK(longitudes, pesos):
+    Longitudes = np.array(longitudes)
+    Pesos = np.array(pesos)
+
+    valoresK = pesos / (longitudes ** 3)
+    return np.mean(valoresK)
